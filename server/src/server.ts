adcf725f01278b6bc00a9a4c2c4c9c9281d04b45
IDMAJOR <kid041709@gmail.com>
2025-04-04 15:42:26 +0100

    cors link

--- server/src/server.ts
+++ server/src/server.ts
@@ -22,7 +22,7 @@ const io = initializeSocket(server);
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://phed-complaints.vercel.app/",
     credentials: true,
   })
 );
